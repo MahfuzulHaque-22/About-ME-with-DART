4f86e69f21476f652d691b16a1a3955c497e8db5
Mahfuzul Haque <68540132+MahfuzulHaque-22@users.noreply.github.com>
2022-11-03 00:40:34 +0600

    Create about_me.dart

---
+++ about_me.dart
@@ -0,0 +1,22 @@
+/// Labas 👋, nice to meet you!
+class AboutMe {
+  final fullName = 'Mahfuzul Haque';
+  final title = 'Software Developer';
+  final location = 'Bogra, Bangladesh🇹';
+  final badges = [
+    'Google Developer for Flutter & Dart',
+  ];
+  final skills = [
+    'Product Development',
+    'Content Creation',
+    'Mentoring',
+    'Googling',
+    'Memes',
+  ];
+  final social = {
+    'Twitter': 'https://twitter.com/',
+    'LinkedIn': 'https://www.linkedin.com/in/',
+    'GitHub': 'https://github.com/',
+    'YouTube': 'https://www.youtube.com/',
+  };
+}
